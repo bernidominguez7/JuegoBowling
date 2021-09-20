4844244e63817e93778d26545bafc03ccf756e34
bernidominguez7 <63733210+bernidominguez7@users.noreply.github.com>
2021-09-20 14:57:13 -0300

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectBowling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectBowling")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5840ba3b-aad3-4cf3-a362-fc2a48b54bc3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using JuegoBowling;
+
+namespace UnitTestProjectBowling
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void JugadaPeor()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Juego.Tirar(0);
+                Juego.Tirar(0);
+            }
+            int resultado = Juego.Score();
+            Assert.AreEqual(resultado, 0);
+        }
+
+        [TestMethod]
+        public void JugadaMediocre()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Juego.Tirar(1);
+                Juego.Tirar(0);
+            }
+            int resultado = Juego.Score();
+            Assert.AreEqual(resultado, 10);
+        }
+
+        [TestMethod]
+        public void JugadaAlMenosSpare()
+        {
+            Juego.Tirar(7);
+            Juego.Tirar(3);
+
+            for (int i = 1; i < 10; i++)
+            {
+                Juego.Tirar(1);
+                Juego.Tirar(0);
+            }
+            int resultado = Juego.Score();
+            Assert.AreEqual(resultado, 20);
+        }
+
+        [TestMethod]
+        public void JugadaAlMenosStrike()
+        {
+            Juego.Tirar(10);
+            Juego.Tirar(0);
+
+            for (int i = 1; i < 10; i++)
+            {
+                Juego.Tirar(1);
+                Juego.Tirar(0);
+            }
+            int resultado = Juego.Score();
+            Assert.AreEqual(resultado, 21);
+        }
+
+        [TestMethod]
+        public void JugadaAlMenosStrikeYSpare()
+        {
+            Juego.Tirar(10);
+            Juego.Tirar(0);
+            Juego.Tirar(7);
+            Juego.Tirar(3);
+
+            for (int i = 2; i < 10; i++)
+            {
+                Juego.Tirar(1);
+                Juego.Tirar(0);
+            }
+            int resultado = Juego.Score();
+            Assert.AreEqual(resultado, 40);
+        }
+
+
+        [TestMethod]
+        public void JugadaTodoSpare()
+        {
+          
+            for (int i = 0; i < 11; i++)
+            {
+                Juego.Tirar(7);
+                Juego.Tirar(3);
+            }
+            int resultado = Juego.Score();
+            Assert.AreEqual(resultado, 200);
+        }
+
+        [TestMethod]
+        public void JugadaTodoStrike()
+        {
+
+            for (int i = 0; i < 12; i++)
+            {
+                Juego.Tirar(10);
+                Juego.Tirar(0);
+            }
+            int resultado = Juego.Score();
+            Assert.AreEqual(resultado, 300);
+        }
+    }
+}
